e9b658356c5a1a85d09f2cc60b1a01c93d18c120
Ikigai <manolispoulakakakis@gmail.com>
2024-03-17 17:51:02 +0200

    Admin user and administration stuff + 1st index.html

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Movie
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+
+    movies = Movie.objects.all()
+    #SELECT * FROM movies_movie
+    #output = ', '.join([m.title for m in movies])
+
+
+    # Movie.objects.filter(release_year=1984)
+    # SELECT * FROM movies_movie WHERE release_year = 1984
+
+    # Movie.objects.get(id=1)
+    # Single Object get
+    # Additionally we could use Raw SQL Statements 
+
+    #return HttpResponse(output)
+    return render(request, 'movies/index.html',{'movies': movies})
