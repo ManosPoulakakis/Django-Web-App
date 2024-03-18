230f3c22ef75fc560a6e3b2fe77908bb1d51d2a8
Ikigai <manolispoulakakakis@gmail.com>
2024-03-18 18:10:53 +0200

    Pre-Deploy

--- movies/views.py
+++ movies/views.py
@@ -1,21 +1,13 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse,Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Movie
-# Create your views here.
+
 
 def index(request):
-
     movies = Movie.objects.all()
-    #SELECT * FROM movies_movie
-    #output = ', '.join([m.title for m in movies])
+    return render(request, 'movies/index.html', {'movies': movies})
 
 
-    # Movie.objects.filter(release_year=1984)
-    # SELECT * FROM movies_movie WHERE release_year = 1984
-
-    # Movie.objects.get(id=1)
-    # Single Object get
-    # Additionally we could use Raw SQL Statements 
-
-    #return HttpResponse(output)
-    return render(request, 'movies/index.html',{'movies': movies})
+def detail(request,movies_id):
+        movie = get_object_or_404(Movie, id=movies_id)
+        return render(request, 'movies/detail.html', {'movie':movie})
